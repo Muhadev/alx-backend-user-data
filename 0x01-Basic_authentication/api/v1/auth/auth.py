778197f77378b790f60c0c4bf1e1265f6d895b6d
Muhadev <muhammedfayemi@gmail.com>
2024-07-09 01:11:41 +0100

    task is added

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,28 @@
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth:
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """
+        This method will later be used to check if a path
+        requires authentication.
+        Currently, it just returns False.
+        """
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        """
+        This method will later be used to get the Authorization
+        header from the request.
+        Currently, it just returns None.
+        """
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """
+        This method will later be used to get the current
+        user from the request.
+        Currently, it just returns None.
+        """
+        return None
