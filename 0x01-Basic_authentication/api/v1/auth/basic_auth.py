c8decffaba09abd5e398969341ae516e2f7f77ff
Muhadev <muhammedfayemi@gmail.com>
2024-07-10 03:20:21 +0100

    task is added

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -9,4 +9,19 @@ class BasicAuth(Auth):
     """
     Basic authentication class that inherits from Auth.
     """
-    pass
+
+    def extract_base64_authorization_header(
+        self,
+        authorization_header: str
+    ) -> str:
+        """
+        Returns the Base64 part of the Authorization
+        header for a Basic Authentication.
+        """
+        if authorization_header is None:
+            return None
+        if not isinstance(authorization_header, str):
+            return None
+        if not authorization_header.startswith("Basic "):
+            return None
+        return authorization_header[len("Basic "):]
