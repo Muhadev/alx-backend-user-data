b6964df62d872fc9cece921fc52ec19bf8495115
Muhadev <muhammedfayemi@gmail.com>
2024-07-11 13:49:38 +0100

    task is added

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python3
-"""Module of session authenticating views.
+"""
+Module of session authenticating views.
 """
 import os
-from typing import Tuple
 from flask import abort, jsonify, request
-
-from models.user import User
 from api.v1.views import app_views
+from api.v1.app import auth
 
 
 @app_views.route('/auth_session/login', methods=['POST'], strict_slashes=False)
@@ -29,9 +28,18 @@ def login() -> Tuple[str, int]:
     if len(users) <= 0:
         return jsonify(not_found_res), 404
     if users[0].is_valid_password(password):
-        from api.v1.app import auth
-        sessiond_id = auth.create_session(getattr(users[0], 'id'))
+        session_id = auth.create_session(getattr(users[0], 'id'))
         res = jsonify(users[0].to_json())
-        res.set_cookie(os.getenv("SESSION_NAME"), sessiond_id)
+        res.set_cookie(os.getenv("SESSION_NAME"), session_id)
         return res
     return jsonify({"error": "wrong password"}), 401
+
+@app_views.route('/auth_session/logout', methods=['DELETE'], strict_slashes=False)
+def logout() -> str:
+    """DELETE /api/v1/auth_session/logout
+    Return:
+      - Empty JSON dictionary
+    """
+    if not auth.destroy_session(request):
+        abort(404)
+    return jsonify({}), 200
