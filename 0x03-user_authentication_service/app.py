75584e47a47446d744611b9120680f8035b80524
Muhadev <muhammedfayemi@gmail.com>
2024-07-17 02:35:41 +0100

    task is fix

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -2,7 +2,7 @@
 """
 entry point module
 """
-from flask import Flask, request, jsonify
+from flask import Flask, jsonify, request, abort
 from auth import Auth
 
 # Create a Flask application
@@ -25,6 +25,25 @@ def users():
         return jsonify({"email": user.email, "message": "user created"})
     except ValueError as e:
         return jsonify({"message": "email already registered"}), 400
+
+
+@app.route('/sessions', methods=['POST'])
+def login():
+    if not request.form or \
+     'email' not in request.form or \
+     'password' not in request.form:
+        abort(400)
+
+    email = request.form['email']
+    password = request.form['password']
+
+    if AUTH.valid_login(email, password):
+        session_id = AUTH.create_session(email)
+        response = jsonify({"email": email, "message": "logged in"})
+        response.set_cookie('session_id', session_id)
+        return response
+    else:
+        abort(401)
 
 
 if __name__ == "__main__":
