13039d686f8fb885b3d675b076524813c950e88c
Muhadev <muhammedfayemi@gmail.com>
2024-07-17 03:39:29 +0100

    task is added

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -110,15 +110,15 @@ def update_password():
     email = request.form.get('email')
     reset_token = request.form.get('reset_token')
     new_password = request.form.get('new_password')
-
-    if not email or not reset_token or not new_password:
-        abort(400)
+    is_password_changed = False
 
     try:
         AUTH.update_password(reset_token, new_password)
+        is_password_changed = True
     except ValueError:
+        is_password_changed = False
+    if not is_password_changed:
         abort(403)
-
     return jsonify({"email": email, "message": "Password updated"})
 
 
